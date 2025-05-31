208d6e003d0b79dc3f9d9d9e7887800effe58c7b
Amin Hosseini <aminhosseini822003@gmail.com>
2025-06-01 01:13:12 +0330

    edit  news models

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class News(models.Model):
     title_news = models.CharField('Title', max_length=50, null=True, blank=True)
-    description_news = models.CharField(max_length=200, null=True, blank=True)  # افزایش طول توصیف
+    description_news = models.TextField(null=True, blank=True)  # افزایش طول توصیف
     image_news = models.ImageField(upload_to='tourr/', null=True, blank=True)  # تغییر نام پوشه به news
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='user_news')  # ارتباط با مدل کاربر
 
